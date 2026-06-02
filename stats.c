062af85944281685f3de6936c10e37a38a677663
Oleksandr <sashapisarchuk@gmail.com>
2026-06-03 01:08:01 +0300

    assignment-1 completed

--- stats.c
+++ stats.c
@@ -9,13 +9,11 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c 
+ * @brief file with function implementations and execution
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Oleksandr Pysarchuk
+ * @date 06.03.2026
  *
  */
 
@@ -38,6 +36,80 @@ void main() {
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
+
+	sort_array(test, SIZE);
+	printf("Sorted array:\n");
+	print_array(test,SIZE);
+
+	print_statistics(test, SIZE);
+
 }
 
 /* Add other Implementation File Code Here */
+
+void print_statistics(unsigned char* array, int array_size) {
+
+	printf("Max value: %d\n", find_maximum(array, array_size));
+	printf("Min value: %d\n", find_minimum(array, array_size));
+	printf("Mean value: %f\n", find_mean(array, array_size));
+	printf("Median value: %d\n", find_median(array, array_size));
+}
+
+void print_array(unsigned char* array, int array_size) {
+	for (int i = 0; i < array_size; i++) {
+		printf("%d\n", array[i]);
+	}
+}
+
+int find_median(unsigned char* array, int array_size) {
+	return (array[array_size / 2 - 1] + array[array_size / 2]) / 2;
+}
+
+float find_mean(unsigned char* array, int array_size) {
+	int tmp = 0;
+	for (int i = 0; i < array_size; i++) {
+		tmp += array[i];			
+	}
+
+	return tmp / (float)array_size;
+}
+
+int find_maximum(unsigned char* array, int array_size) {
+	unsigned char tmp = 0; 
+
+	tmp = array[0];
+
+	for (int i = 0; i < array_size; i++) {
+		if (array[i] > tmp) {
+			tmp = array[i];
+		}
+	}
+
+	return tmp;
+}
+
+int find_minimum(unsigned char* array, int array_size) {
+	unsigned char tmp = 0;
+
+	tmp = array[0];
+
+	for (int i = 0; i < array_size; i++) {
+		if (array[i] < tmp) 
+			tmp = array[i];
+	}
+
+	return tmp;
+}
+
+void sort_array(unsigned char* array, int array_size) {
+	int tmp = 0;
+	for (int i = 0; i < array_size - 1; i++) {
+		for (int j = 0; j < array_size - i - 1; j++) {
+			if (array[j] < array[j + 1]) {
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;	
+			}
+		}
+	}
+}
--- stats.h
+++ stats.h
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief function header file
  *
- * <Add Extended Description Here>
+ * Function header file for stats.c file 
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Pysarchuk
+ * @date 06.03.2026
  *
  */
 #ifndef __STATS_H__
@@ -36,5 +36,86 @@
  * @return <Add Return Informaiton here>
  */
 
+/**
+ * @brief Print statistics about array 
+ * This function takes array pointer and array size as parameters
+ * and prints max, min, mean and median value of an array.
+ *
+ * @param pointer to an array that will be used to print statistics about
+ * @param array size, because array is statically allocated
+ *
+ * @return function return nothing, all statistics is printed to console
+ */
+void print_statistics(unsigned char* array, int array_size);
+
+/**
+ * @brief Print all array elements to console
+ * This function prints each element of array in newline
+ *
+ * @param pointer to an array that will be printed
+ * @param array size, because array is statically allocated
+ *
+ * @return return nothing, all array elements are printed to console
+ */
+void print_array(unsigned char* array, int array_size);
+
+/**
+ * @brief Find the median value of an array
+ * This function takes array as an input and find its median value
+ *
+ * @param pointer to an array, where function will look for median value
+ * @param array size, because array is statically allocated
+ *
+ * @return return integer value of an array median value
+ */
+int find_median(unsigned char* array, int array_size);
+
+/**
+ * @brief Find mean valie of an array 
+ * This function takes array as input and find its mean value
+ *
+ * @param pointer to an array, where function will calculate mean value
+ * @param array size, because array is statically allocated
+ *
+ * @return mean calculation result as floating point value
+ */
+
+float find_mean(unsigned char* array, int array_size);
+
+/**
+ * @brief Find maximum element of given array
+ * This function takes array as parameter and returns its maximum value
+ *
+ * @param pointer to an array, where function will look for maximum value
+ * @param array size, because array is statically allocated
+ *
+ * @return maximum element of an array as integer value
+ */
+
+int find_maximum(unsigned char* array, int array_size);
+
+/**
+ * @brief Find minimum element of given array
+ * This function takes array as parameter and returns its minimum value
+ *
+ * @param pointer to an array, where function will look for minimum value
+ * @param array size, because array is statically allocated
+ *
+ * @return minimum element of an array as integer value
+ */
+
+int find_minimum(unsigned char* array, int array_size);
+
+/**
+ * @brief Sort the given array in descending order
+ * This function takes array as input and sort its elements in descending order 
+ *
+ * @param pointer to an array, which will be sorted
+ * @param array size, because array is statically allocated
+ *
+ * @return nothing is returned because functio task is to change the array values order
+ */
+
+void sort_array(unsigned char* array, int array_size);
 
 #endif /* __STATS_H__ */
